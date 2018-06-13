afbcf3c63ef7ddb0e4e6c004a6ea88d3a36820d9
Yurii Izio <yurikizio@gmail.com>
2018-06-13 22:17:41 +0300

    Add project files.

---
+++ lab1azure/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(lab1azure.Startup))]
+namespace lab1azure
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
